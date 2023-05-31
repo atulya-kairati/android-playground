fbdcada64de27195d35f6989d6551a91c4674b0a
atulya-kairati <atulyakairati@gmail.com>
2023-05-31 23:34:35 +0530

    expandable list basic

---
+++ ExpandableListWithOnboardingScreen/app/src/main/java/com/atulya/expandablelistwithonboardingscreen/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.atulya.expandablelistwithonboardingscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.atulya.expandablelistwithonboardingscreen.ui.theme.ExpandableListWithOnboardingScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExpandableListWithOnboardingScreenTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+
+    val names: List<String> = listOf("Manus", "Compose")
+
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.padding(4.dp)
+        ) {
+            names.forEach { name ->
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    var isExpanded: Boolean by remember {
+        mutableStateOf(false)
+    }
+
+    val extraPadding = if(isExpanded) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = modifier.padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier.weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(
+                    text = "Hello,",
+                )
+                Text(
+                    text = name,
+                )
+            }
+
+            ElevatedButton(onClick = { isExpanded = !isExpanded }) {
+                Text(text = if(isExpanded) "Show less" else "Show More")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExpandableListWithOnboardingScreenTheme {
+        MyApp()
+    }
+}
