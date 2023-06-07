cdd7d990e2144e5256e8dba1a837234a9251d0c3
atulya-kairati <atulyakairati@gmail.com>
2023-06-07 16:37:48 +0530

    remember and mutable state

---
+++ ComposeStateCodelab/app/src/main/java/com/atulya/composestatecodelab/ui/components/WaterCounter.kt
@@ -0,0 +1,40 @@
+package com.atulya.composestatecodelab.ui.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun WaterCounter(
+    modifier: Modifier = Modifier,
+) {
+    // this alone will not work since count will keep getting reassigned to 0 during recomposition
+    // we need to use remember in conjunction so that old value of count is remembered during
+    // recomposition
+    // val count: MutableState<Int> = mutableStateOf(0)
+
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "You've had ${count} glasses.",
+        )
+
+        Button(onClick = { count++ }) {
+            Text(text = "Add one")
+        }
+    }
+}
---
+++ ComposeStateCodelab/app/src/main/java/com/atulya/composestatecodelab/ui/components/WellnessScreen.kt
@@ -0,0 +1,11 @@
+package com.atulya.composestatecodelab.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier
+){
+    WaterCounter(modifier = modifier)
+}
