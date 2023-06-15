057d01dadee5eac803bdc18cad82c56ee7ef9977
atulya-kairati <atulyakairati@gmail.com>
2023-06-15 12:16:26 +0530

    LazyRow example

---
+++ LazyLayouts/app/src/main/java/com/atulya/lazylayouts/ui/components/CardItems.kt
@@ -0,0 +1,116 @@
+package com.atulya.lazylayouts.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.atulya.lazylayouts.R
+
+@Composable
+fun HorizontalCard(
+    modifier: Modifier = Modifier,
+    cardColors: CardColors = CardDefaults.cardColors()
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        colors = cardColors,
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.putar),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(64.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.FillBounds
+            )
+
+
+            Column {
+                Text(
+                    text = "Big ass title",
+                    style = MaterialTheme.typography.titleSmall,
+                )
+                Text(
+                    text = "Puny lorem ipsum dolor manus chaubey bing chilling",
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SquareCard(
+    modifier: Modifier = Modifier,
+    cardColors: CardColors = CardDefaults.cardColors()
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        colors = cardColors,
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.putar),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(120.dp),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = "Hari Putar",
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier
+                    .paddingFromBaseline(top = 24.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFCDDC39)
+@Composable
+fun HorizontalCardPreview() {
+    HorizontalCard(
+        modifier = Modifier.padding(4.dp)
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFCDDC39)
+@Composable
+fun SquareCardPreview() {
+    SquareCard(
+        modifier = Modifier.padding(4.dp)
+    )
+}
---
+++ LazyLayouts/app/src/main/java/com/atulya/lazylayouts/ui/components/LazyRowExample.kt
@@ -0,0 +1,74 @@
+package com.atulya.lazylayouts.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@Composable
+fun LazyRowExample(
+    modifier: Modifier = Modifier,
+) {
+
+    val state = rememberLazyListState() // to remember scroll position across recomposition
+    val coroutineScope = rememberCoroutineScope()
+
+    val showStaringItem by remember {
+        derivedStateOf {
+            state.layoutInfo.totalItemsCount > 4
+        }
+    }
+
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp), // space between children
+        contentPadding = PaddingValues(8.dp), // padding without clipping children
+        verticalAlignment = Alignment.CenterVertically,
+        state = state
+    ) {
+        items(10) {
+            SquareCard(
+                cardColors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+
+        if (showStaringItem) {
+            item {
+                IconButton(
+                    onClick = {
+                        coroutineScope.launch {
+                            // scroll to start
+                            state.animateScrollToItem(index = 0)
+                        }
+                    },
+                ) {
+                    Icon(imageVector = Icons.Default.KeyboardArrowLeft, contentDescription = null)
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, backgroundColor = 0xFFCDDC39)
+@Composable
+fun LazyRowExamplePreview() {
+    LazyRowExample()
+}
